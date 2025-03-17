319ed4f299897f1a584dd007bb08cc3479eb2d2d
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2025-03-17 09:02:00 -0700

    chore: extract handled message list to shared place

--- src/wsd/udp/mod.rs
+++ src/wsd/udp/mod.rs
@@ -1,2 +1,11 @@
 pub mod client;
 pub mod host;
+
+use std::sync::{Arc, LazyLock};
+
+use tokio::sync::RwLock;
+
+use crate::max_size_deque::MaxSizeDeque;
+
+static HANDLED_MESSAGES: LazyLock<Arc<RwLock<MaxSizeDeque<String>>>> =
+    LazyLock::new(|| Arc::new(RwLock::new(MaxSizeDeque::new(10))));
