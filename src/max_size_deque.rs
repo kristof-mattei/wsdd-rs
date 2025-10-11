6c209096fed78fed44e7d62c52a3db7560711670
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2025-10-11 08:32:25 -0700

    chore(coverage): added more tests for max_size_deque

--- src/max_size_deque.rs
+++ src/max_size_deque.rs
@@ -23,21 +23,76 @@ impl<T> MaxSizeDeque<T> {
     }
 
     pub fn push_back(&mut self, value: T) {
+        self.inner.push_back(value);
+
         if self.inner.len() > self.max_size {
             self.inner.pop_front();
         }
-
-        self.inner.push_back(value);
     }
 
-    #[expect(unused, reason = "WIP")]
+    #[cfg_attr(not(test), expect(unused, reason = ""))]
     pub fn push_front(&mut self, value: T) {
+        self.inner.push_front(value);
+
         if self.inner.len() > self.max_size {
             self.inner.pop_back();
         }
-
-        self.inner.push_front(value);
     }
 }
 
 impl<T> MaxSizeDeque<T> where T: Eq {}
+
+#[cfg(test)]
+mod tests {
+    use crate::max_size_deque::MaxSizeDeque;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn no_slots() {
+        let mut max_size_deque = MaxSizeDeque::<usize>::new(0);
+
+        assert!(max_size_deque.inner.is_empty());
+
+        max_size_deque.push_back(5);
+
+        assert!(max_size_deque.inner.is_empty());
+    }
+
+    #[test]
+    fn push_back_deletes_from_front() {
+        let mut max_size_deque = MaxSizeDeque::<usize>::new(5);
+        assert!(max_size_deque.inner.is_empty());
+
+        for i in 0..5 {
+            max_size_deque.push_back(i);
+        }
+
+        assert_eq!(max_size_deque.inner.len(), 5);
+
+        max_size_deque.push_back(5);
+
+        assert_eq!(max_size_deque.inner.len(), 5);
+
+        // we expect 0 to have fallen off
+        assert!(max_size_deque.iter().any(|v| *v != 0));
+    }
+
+    #[test]
+    fn push_front_deletes_from_back() {
+        let mut max_size_deque = MaxSizeDeque::<usize>::new(5);
+        assert!(max_size_deque.inner.is_empty());
+
+        for i in 1..6 {
+            max_size_deque.push_back(i);
+        }
+
+        assert_eq!(max_size_deque.inner.len(), 5);
+
+        max_size_deque.push_front(0);
+
+        assert_eq!(max_size_deque.inner.len(), 5);
+
+        // we expect 0 to have fallen off
+        assert!(max_size_deque.iter().any(|v| *v != 6));
+    }
+}
