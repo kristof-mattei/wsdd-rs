dcdb91b903a51469f118dd973e1fb0bce79904c2
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2025-10-11 13:31:55 -0700

    chore(coverage): increased coverage

--- src/network_address.rs
+++ src/network_address.rs
@@ -14,15 +14,16 @@ impl NetworkAddress {
         Self { address, interface }
     }
 
-    // """ return true if the (interface) address can be used for creating (link-local) multicasting sockets  """
+    // Can the address can be used for creating (link-local) multicasting sockets?
     // # Nah, this check is not optimal but there are no local flags for
     // # addresses, but it should be safe for IPv4 anyways
     // # (https://tools.ietf.org/html/rfc5735#page-3)
-    // return ((self._family == socket.AF_INET) and (self._raw_address[0] != 127)
-    //         or (self._family == socket.AF_INET6) and (self._raw_address[0:2] == b'\xfe\x80'))
     pub fn is_multicastable(&self) -> bool {
         match self.address {
-            IpAddr::V4(ipv4_addr) => !ipv4_addr.is_loopback(),
+            IpAddr::V4(ipv4_addr) => {
+                // TODO add `https://doc.rust-lang.org/std/net/enum.IpAddr.html#method.is_global` once stabilized
+                !ipv4_addr.is_loopback()
+            },
             IpAddr::V6(ipv6_addr) => ipv6_addr.is_unicast_link_local(),
         }
     }
@@ -37,5 +38,63 @@ impl std::fmt::Display for NetworkAddress {
 impl std::cmp::PartialEq for NetworkAddress {
     fn eq(&self, other: &Self) -> bool {
         self.address == other.address && self.interface == other.interface
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        net::{Ipv4Addr, Ipv6Addr},
+        sync::Arc,
+    };
+
+    use libc::RT_SCOPE_SITE;
+
+    use crate::{network_address::NetworkAddress, network_interface::NetworkInterface};
+
+    #[test]
+    fn ipv4_loopback_not_multicastable() {
+        let network_address = NetworkAddress::new(
+            Ipv4Addr::new(127, 1, 2, 3).into(),
+            Arc::new(NetworkInterface::new_with_index("eth0", RT_SCOPE_SITE, 5)),
+        );
+
+        assert!(!network_address.is_multicastable());
+    }
+
+    #[test]
+    fn ipv4_local_multicastable() {
+        let network_address = NetworkAddress::new(
+            Ipv4Addr::new(192, 168, 100, 5).into(),
+            Arc::new(NetworkInterface::new_with_index("eth0", RT_SCOPE_SITE, 5)),
+        );
+
+        assert!(network_address.is_multicastable());
+    }
+
+    #[test]
+    fn ipv6_global_not_multicastable() {
+        let network_address = NetworkAddress::new(
+            Ipv6Addr::new(
+                0x2001, 0x0db8, 0x5c41, 0xf105, 0x2cf9, 0xcd58, 0x0b74, 0x0684,
+            )
+            .into(),
+            Arc::new(NetworkInterface::new_with_index("eth0", RT_SCOPE_SITE, 5)),
+        );
+
+        assert!(!network_address.is_multicastable());
+    }
+
+    #[test]
+    fn ipv6_link_local_multicastable() {
+        let network_address = NetworkAddress::new(
+            Ipv6Addr::new(
+                0xfdd5, 0x8f27, 0x6bc0, 0xf625, 0xcb86, 0xd38b, 0xa939, 0x3c67,
+            )
+            .into(),
+            Arc::new(NetworkInterface::new_with_index("eth0", RT_SCOPE_SITE, 5)),
+        );
+
+        assert!(!network_address.is_multicastable());
     }
 }
