46920d1d8d36161cce36d85a7e0238f53e44f11d
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2025-11-16 22:06:16 -0700

    fix: be explicit that sd_listen_fds takes an i32

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,11 +1,11 @@
 pub fn listen_fds(unset_environment: bool) -> Result<Vec<i32>, std::io::Error> {
     #[link(name = "systemd")]
     unsafe extern "C" {
-        fn sd_listen_fds(unset_environment: bool) -> i32;
+        fn sd_listen_fds(unset_environment: i32) -> i32;
     }
 
     // SAFETY: normal ffi call
-    let result = unsafe { sd_listen_fds(unset_environment) };
+    let result = unsafe { sd_listen_fds(unset_environment.into()) };
 
     #[expect(clippy::single_match_else, reason = "Clarity")]
     match result {
