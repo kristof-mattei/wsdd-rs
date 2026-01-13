d72824680aea90a9070665e449508e9fff3fb2ac
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2026-01-12 23:41:37 -0700

    fix: clean up moved macro

--- src/ffi.rs
+++ src/ffi.rs
@@ -34,33 +34,6 @@ pub fn listen_fds(_unset_environment: bool) -> Result<Vec<i32>, std::io::Error> 
     Ok(vec![])
 }
 
-#[macro_export]
-macro_rules! define_typed_size {
-    ($name:ident, $target_type:ty, $source:ty) => {
-        const $name: $target_type = const {
-            let size = core::mem::size_of::<$source>();
-
-            if size > <$target_type>::MAX as usize {
-                panic!(concat!(
-                    "The size of `",
-                    stringify!($source),
-                    "` exceeds the capacity of type `",
-                    stringify!($target_type),
-                    "`"
-                ));
-            }
-
-            #[expect(
-                clippy::cast_possible_truncation,
-                reason = "We validate that the size fits in the target size"
-            )]
-            {
-                size as $target_type
-            }
-        };
-    };
-}
-
 #[cfg(test)]
 mod tests {
     #[test]
