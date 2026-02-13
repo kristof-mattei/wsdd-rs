4e9bdfb8d6f0539e979a57c3ad16911f9da9f57b
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2026-02-13 13:30:36 -0700

    fix: typo

--- crates/wsdd-rs/src/soap/parser/generic.rs
+++ crates/wsdd-rs/src/soap/parser/generic.rs
@@ -35,7 +35,7 @@ where
                 }
             },
             element @ XmlEvent::EndDocument => {
-                return Err(GenericParsingError::UnspectedEvent(Box::new(element)));
+                return Err(GenericParsingError::UnexpectedEvent(Box::new(element)));
             },
             _ => {
                 // these events are squelched by the parser config, or they're valid, but we ignore them
@@ -101,7 +101,7 @@ where
                 }
             },
             element @ XmlEvent::EndDocument => {
-                return Err(GenericParsingError::UnspectedEvent(Box::new(element)));
+                return Err(GenericParsingError::UnexpectedEvent(Box::new(element)));
             },
             _ => {
                 // these events are squelched by the parser config, or they're valid, but we ignore them
